bad838bc6774a8c0e825f2dd42e9855cf91c9e1f
VedantCMS <vedantgupta.cms@gmail.com>
2024-01-16 14:27:37 +0530

    med

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
-jQuery(window).scroll(function () {
-  if (jQuery(this).scrollTop() > 270) {
-    jQuery(".mainheader").addClass("fixed-header");
-  } else {
-    jQuery(".mainheader").removeClass("fixed-header");
-  }
+document.addEventListener("DOMContentLoaded", function () {
+
+  window.addEventListener('scroll', function () {
+
+    if (window.scrollY > 300) {
+      document.getElementById('navbar_top').classList.add('fixed-top');
+      // add padding top to show content behind navbar
+      navbar_height = document.querySelector('.navbar').offsetHeight;
+      document.body.style.paddingTop = navbar_height + 'px';
+    } else {
+      document.getElementById('navbar_top').classList.remove('fixed-top');
+      // remove padding top from body
+      document.body.style.paddingTop = '0';
+    }
+  });
 });
 
 jQuery(document).ready(function () {
@@ -82,6 +91,29 @@ var owl = $("#globalAdisory").owlCarousel({
     },
   },
 });
+
+$('.hero-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  dots: false,
+  navText: ['<i class="fa-solid fa-angle-left"></i>', '<i class="fa-solid fa-angle-right"></i>'],
+  autoplay: true,
+  autoplayTimeout: 7000,
+  smartSpeed: 800,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 1
+    }
+  }
+})
+
 
 function scrollToCarousel(index) {
   owl.trigger("to.owl.carousel", index);
